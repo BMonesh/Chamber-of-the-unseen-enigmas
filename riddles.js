4b8048186318a778ec9b0e9ebd830a4713781fc0
BMonesh <141898445+BMonesh@users.noreply.github.com>
2023-11-10 10:24:31 +0530

    Add files via upload

--- riddles.js
+++ riddles.js
@@ -1,91 +1,31 @@
 export const riddlesSet1 = [
-    {
-      riddle: "I'm the entity that emerges from a cursed mirror, seeking to claim souls. What am I?",
-      answer: "Reflect"
-    },
-    {
-      riddle: "I'm the eerie phenomenon where time repeats itself, often with tragic consequences. What am I?",
-      answer: "Loop"
-    },
-    {
-      riddle: "I'm the ancient, slumbering beast that rises from the depths of the ocean to wreak havoc. What am I?",
-      answer: "Leviathan"
-    },
-    {
-      riddle: "I'm the malevolent force that possesses a child's toy and turns it into a harbinger of doom. What am I?",
-      answer: "Puppet"
-    },
-    {
-      riddle: "I'm the sinister carnival that appears in the dead of night, promising dark delights. What am I called?",
-      answer: "Carnivale"
-    },
-  ];
+  { riddle: "I lurk in the shadows, a creature of dread, with teeth sharp as razors, and eyes glowing red. What am I?", answer: "Vampire" },
+  { riddle: "In the darkness they dwell, silent and sly, with pointed fangs and a haunting cry. What are they?", answer: "Bats" },
+  { riddle: "I have no bones, no body, no form. I come in the night, bringing a storm. What am I?", answer: "Ghost" },
+  { riddle: "In the attic, I creak and moan, a specter of doom, forever alone. What am I?", answer: "Haunted house" },
+  { riddle: "A puzzle of fear, a maze of fright, solve me if you can, in the pale moonlight. What am I?", answer: "Labyrinth" }
+];
   
  export const riddlesSet2 = [
-    {
-      riddle: "I'm the chilling apparition of a woman in white, said to foretell death to those who see her. What am I?",
-      answer: "Banshee"
-    },
-    {
-      riddle: "I'm the cursed town where every night is a nightmarish carnival of horrors. What town am I?",
-      answer: "Ravenswood"
-    },
-    {
-      riddle: "I'm the eerie phenomenon where footsteps echo in an empty building when no one is around. What am I?",
-      answer: "Footfalls"
-    },
-    {
-      riddle: "I'm the restless spirit of a wronged man, seeking to reveal the truth behind his untimely demise. What am I?",
-      answer: "Specter"
-    },
-    {
-      riddle: "I'm the supernatural entity that manifests in your dreams, turning them into nightmares. What am I called?",
-      answer: "Dreamweaver"
-    },
-  ];
+  { riddle: "I'm a mirror that shows your deepest fear, reflecting nightmares that draw near. What am I?", answer: "Nightmare mirror" },
+  { riddle: "Crawling and creeping, with eight legs to spare, in the corner, I weave a web of despair. What am I?", answer: "Spider" },
+  { riddle: "I have keys but open no locks, I speak but make no sound. In the darkness, I am often found. What am I?", answer: "Ouija board" },
+  { riddle: "I walk without legs, I scream without a mouth. Wherever I go, darkness follows. What am I?", answer: "Shadow" },
+  { riddle: "A game of hide and seek, where the seeker never sleeps. What is it?", answer: "Slenderman" }
+];
   
  export const riddlesSet3 = [
-    {
-      riddle: "I'm the embodiment of fear, lurking in the shadows of your deepest nightmares. What am I?",
-      answer: "Dread"
-    },
-    {
-      riddle: "I'm the malevolent entity that haunts a cursed painting, compelling people to commit unspeakable acts. What am I?",
-      answer: "Haunting"
-    },
-    {
-      riddle: "I'm the forbidden tome filled with dark incantations and eldritch knowledge. What am I called?",
-      answer: "Grimoire"
-    },
-    {
-      riddle: "I'm the eerie child who speaks to imaginary friends that are anything but imaginary. What is my name?",
-      answer: "Poltergeist"
-    },
-    {
-      riddle: "I'm the small town with a history of unexplained disappearances and strange occurrences. Where am I?",
-      answer: "Silent Hill"
-    },
-  ];
+  { riddle: "In the graveyard, I lie still, a marker of death on a cold, stone hill. What am I?", answer: "Tombstone" },
+  { riddle: "With each step, I echo your dread, a phantom presence beside your bed. What am I?", answer: "Footsteps" },
+  { riddle: "I have a hundred eyes, yet cannot see. I live in the shadows, watching silently. What am I?", answer: "Portrait" },
+  { riddle: "Born in the crypt, I hunger for flesh, with an insatiable appetite that causes unrest. What am I?", answer: "Zombie" },
+  { riddle: "I whisper in the wind, a voice from beyond, telling tales of darkness that will respond. What am I?", answer: "Ghostly whispers" }
+];
   
  export const riddlesSet4 = [
-    {
-      riddle: "I'm the eerie hotel with a haunted room that nobody dares to enter. What's my name?",
-      answer: "Overlook"
-    },
-    {
-      riddle: "I'm the vengeful spirit of a woman wronged in life, seeking retribution in the afterlife. What am I?",
-      answer: "Banshee"
-    },
-    {
-      riddle: "I'm the cursed artifact said to bring death to anyone who possesses it. What am I?",
-      answer: "Phylactery"
-    },
-    {
-      riddle: "I'm the shape-shifting creature that torments a group of friends in an isolated location. What am I?",
-      answer: "Wendigo"
-    },
-    {
-      riddle: "I'm the mysterious entity that can possess people, driving them to commit unspeakable acts of violence. What am I called?",
-      answer: "Malevolence"
-    },
-  ];
+  { riddle: "A puzzle of gore, a maze of despair, follow my clues if you dare. What am I?", answer: "Blood trail" },
+  { riddle: "I am a door without a key, a gateway to the unknown. Enter if you dare, but you may not return home. What am I?", answer: "Portal" },
+  { riddle: "I lurk in the swamp, with scales and a hiss, a monstrous creature you can't dismiss. What am I?", answer: "Swamp creature" },
+  { riddle: "I wear a mask of terror, my laughter a scream. In your nightmares, I am the king or queen. What am I?", answer: "Nightmare clown" },
+  { riddle: "I am a game where death is the prize, each move you make, a step toward demise. What am I?", answer: "Deadly chess" }
+];
